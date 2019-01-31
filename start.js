34a94a191725c276b41bac21c48193389b7b3d46
d-nagy <danielnagy69@gmail.com>
2019-01-31 09:19:36 +0000

    Setup heroku

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 
+const PORT = process.env.PORT || 3000
 
-const server = app.listen(3000, () => {
+const server = app.listen(PORT, () => {
     console.log(`App is running on port ${server.address().port}`);
 });
